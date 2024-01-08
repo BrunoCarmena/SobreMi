49a1ce4a9919e203a2ba41b180f4da9c196fd493
bruno <bruno.s.carmena@hotmail.com>
2024-01-08 14:54:22 -0300

    funciones en botones

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle ("sticky", window.scrollY > 100);
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 };
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 }
+
+function toggleDisplay(elementId, buttonText) {
+    var element = document.getElementById(elementId);
+    element.style.display = (element.style.display === 'none' || element.style.display === '') ? 'block' : 'none';
+    document.querySelector(buttonText).textContent = (element.style.display === 'none') ? 'Leer Más' : 'Leer Menos';
+}
+
+function mostrarMasInfo() {
+    toggleDisplay('parrafoOculto', '.btn');
+}
+
+function mostrarTextoCompleto() {
+    toggleDisplay('textoCompleto', '.read');
+}
+
+function verTexto() {
+    toggleDisplay('txtCompleto', '.read');
+}
+
+function mostrarMas() {
+    toggleDisplay('txtEntero', '.read');
+}
+
+
